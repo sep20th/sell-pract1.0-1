a8a2897e542e1e6d4a4fbc69a143d0afff66c334
sep20th <sep20th@qq.com>
2017-04-29 21:09:45 +0800

    左右联动未好

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,225 @@
 <template>
-	<div>
-		<h1>im goods</h1>
+	<div class="goods">
+		<div class="menu-wrapper">
+			<ul>
+				<li v-for="item in goods" class="menu-item" :class="{'current':currentIndex === $index}">
+					<span class="text border-1px">
+						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+					</span>
+				</li>
+			</ul>
+		</div>
+		<div class="foods-wrapper" v-el:foods-wrapper :scoll='onScroll'>
+			<ul>
+				<li v-for="item in goods" class = "food-list food-list-hook">
+					<h1 class="title">{{item.name}}</h1>
+					<ul>
+						<li class="food-item border-1px" v-for="food in item.foods" >
+							<div class="icon">
+								<img :src="food.icon" width="57" height="57">
+							</div>
+							<div class="content">
+								<h2 class="name">{{food.name}}</h2>
+								<p class="desc">{{food.description}}</p>
+								<div class="extra">
+									<span class="count">月售{{food.sellCount}}份</span>
+									<span>好评率{{food.rating}}%</span>
+								</div>
+								<div class="price">
+									<span class="now">￥{{food.price}}</span>
+									<span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+
+		</div>
 	</div>
 </template>
 
 <script>
+	const ERR_OK = 0;
 	export default{
-		
+		props:{
+			seller:{
+				type:Object
+			}
+		},//props end
+
+		data(){
+			return{
+				goods:[],
+				listHeight:[],
+				scollY:0
+
+			}
+		},//data end
+
+		created(){
+			this.$http.get('api/goods').then((response)=>{
+				response = response.body;
+				if(response.errno === ERR_OK){
+					this.goods = response.data;
+					this.$nextTick(()=>{
+						this._calculateHeight();
+					//监听滚动事件
+						window.addEventListener('scroll', this.onScroll)
+					})
+				}
+			});
+			this.classMap = ['decrease','discount','special','invoice','guarantee'];
+		},//created end
+
+		computed:{
+			currentIndex(){
+				for(let i=0;i<this.listHeight.length;i++){
+					let height1 = this.listHeight[i];
+					let height2 = this.listHeight[i+1];
+					if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)){
+						return i;
+					}
+				}
+				return 0;
+			},
+			
+
+		},//computed end
+
+		methods:{
+			_initScroll(){},
+
+			//计算food-list高度
+			_calculateHeight(){
+				let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+				let height = 0;
+				this.listHeight.push(height)
+				for(let i=0;i<foodList.length;i++){
+					let item = foodList[i];
+					height += item.clientHeight;
+					this.listHeight.push(height)
+				}
+			},
+
+			onScroll(){
+				console.log('111')
+			},
+		},//methods end
+
 	}
 </script>
+
+<style lang="stylus">
+	@import "../../common/stylus/mixin.styl";
+
+	.goods
+		position:absolute
+		display:flex
+		width:100%
+		top:174px
+		bottom:46px
+		overflow:hidden
+		.menu-wrapper
+			flex:0 0 80px
+			width:80px
+			background:#f3f5f7;
+			overflow-y: scroll;
+			-webkit-overflow-scrolling: touch
+			&::-webkit-scrollbar
+				display: none
+			.menu-item
+				display:table
+				height:54px
+				width:56px
+				line-height:14px
+				padding:0 12px
+				&.current
+					position:relative
+					margin-top:-1px
+					background:#fff 
+					font-weight:700
+					.text
+						border-none()
+				.icon
+					display:inline-block
+					vertical-align:top;
+					width:12px
+					height:12px
+					margin-right:2px
+					background-size:12px
+					background-repeat: no-repeat
+					&.decrease
+						bg-image('decrease_3')
+					&.discount
+						bg-image('discount_3')
+					&.guarantee
+						bg-image('guarantee_3')
+					&.invoice
+						bg-image('invoice_3')
+					&.special
+						bg-image('special_3')	
+				.text
+					display:table-cell
+					width:56px
+					vertical-align:middle
+					border-1px(rgba(7,17,27,0.1))
+					font-size:12px
+		.foods-wrapper
+			flex:1
+			overflow-y: scroll
+			-webkit-overflow-scrolling: touch
+			&::-webkit-scrollbar
+				display: none
+			.title
+				padding-left:14px
+				height:26px
+				line-height:26px
+				border-left:2px solid #d9dde1
+				font-size:12px
+				color:rgb(147,153,159)
+				background:#f3f5f7
+			.food-item
+				display:flex
+				margin:18px
+				padding-bottom:18px
+				border-1px(rgba(7,17,27,0.1))	
+				&:last-child
+					border-none()
+					margin-bottom:0
+				.icon
+					flex:0 0 57px
+					margin-right:10px
+				.content
+					flex:1
+					.name
+						margin:2px 0 8px 0
+						height:14px
+						line-height:14px
+						font-size:14px
+						color:rgb(7,17,27)
+					.desc,.extra
+						line-height: 10px
+						font-size: 10px
+						color:rgb(147,153,159)	
+					.desc
+						line-height:12px
+						margin-bottom:8px	
+					.extra
+						line-height:10px
+						&.count
+							margin-right:12px
+					.price
+						font-weight:700
+						line-height:24px
+						.now
+							margin-right:8px
+							font-size:14px
+							color:rgb(240,20,20)
+						.old
+							text-decoration:line-thorgh
+							font-size:10px
+							color:rgb(147,153,159)	
+								
+
+</style>
