c3a859a2a52ae10748a3d0008bafc023c120e2be
sep20th <sep20th@qq.com>
2017-05-01 11:45:40 +0800

    商品列表完成 --(小球动画未做)

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,7 +1,7 @@
 <template>
-	<section class="shopcart">
-		<div class="content">
-			<div class="content-left">
+	<section class="shopcart" @click="toggleList">
+		<div class="content" >
+			<div class="content-left" >
 				<div class="logo-wrapper">
 					<div class="logo" :class = "{'heighlight':totalCount>0}">
 						<i class="icon-shopping_cart" :class = "{'heighlight':totalCount>0}"></i>
@@ -11,15 +11,74 @@
 				<div class="price" :class = "{'heighlight':totalPrice>0}">￥{{totalPrice}}</div>
 				<div class="desc">另需配送费￥{{deliveryPrice}}元</div>
 			</div>
-			<div class="content-right">
+			<!-- 使用 .stop.prevent阻止点击事件冒泡-->
+			<div class="content-right" @click.stop.prevent="pay">
 				<div class="pay" :class="payClass">{{payDesc}}</div>
 			</div>
 		</div>
-
+		<div class="shopcart-list" v-show="listShow" transition="fold">
+			<div class="list-header">
+				<h1 class="title">购物车</h1>
+				<span class="empty" @click='empty'>清空</span>
+			</div>
+			<div class="list-content">
+				<ul>
+					<li class="food" v-for='food in selectFoods'>
+						<span class="name">{{food.name}}</span>
+						<div class="price">
+							<span>￥{{food.price*food.count}}</span>
+						</div>
+						<div class="cartcontrol-wrapper">
+							<cartcontrol :food="food"></cartcontrol>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</section>
+	<div class="list-mask" @click='hideList' v-show = "listShow" transition="fade"></div>
 </template>
 <script>
+	import cartcontrol from 'components/cartcontrol/cartcontrol' 
+
 	export default{
+		data(){
+			return{
+				fold:true
+			}
+		},//data end
+
+		methods:{
+			//显示-收起列表
+			toggleList(){
+				if(!this.totalCount){
+					return
+				};
+				this.fold = !this.fold
+			},
+
+			//清空购物车
+			empty(){
+				this.selectFoods.forEach((food)=>{
+					food.count = 0;
+				})
+			},
+
+			//收起列表
+			hideList(){
+				this.fold = true
+			},
+
+			//去结算
+			pay(){
+				if(this.totalPrice<this.minPrice){
+					return;
+				}else{
+					window.alert(`支付${this.totalPrice}元`)
+				}
+			}
+		},//methods end
+
 		//接收父组件传递的值
 		props:{
 			selectFoods:{
@@ -80,11 +139,27 @@
 				}else{
 					return "enough"
 				}
+			},//payclass end
+
+			//显示隐藏购物车详情
+			listShow(){
+				if(!this.totalCount){
+					this.fold = true;
+					return false
+				}
+				let show = !this.fold
+				return show
 			}
 		},//computed end
+
+		components:{
+			cartcontrol
+		}
 	};
 </script>
 <style lang="stylus">
+	@import "../../common/stylus/mixin.styl";
+	
 	.shopcart
 		position:fixed
 		left:0
@@ -171,4 +246,72 @@
 					&.enough
 						background:#00b43c
 						color:#fff	
+		.shopcart-list
+			position:absolute
+			top:0
+			left:0
+			z-index:-1	
+			width:100%
+			overflow:hidden
+			&.fold-transition
+				transition:all 0.5s
+				transform:translate3d(0,-100%,0)
+			&.fold-enter,&.fold-leave
+				transform:	translate3d(0,0,0)
+			.list-header
+				height:40px
+				line-height:40px
+				padding:0 18px
+				background:#f3f5f7
+				border-bottom:1px solid rgba(7,17,27,0.1)	
+				.title
+					float:left
+					font-size:14px
+					color:rgb(7,17,27)
+				.empty
+					float:right	
+					font-size:12px
+					color:rgb(0,160,220)
+			.list-content
+				padding:0 18px
+				max-height:217px
+				background:#fff
+				overflow-y:scroll
+				&::-webkit-scrollbar
+					display: none	
+				.food
+					position:relative
+					padding:12px 0
+					box-sizing:border-box
+					border-1px(rgba(7,17,27,0.1))	
+					.name
+						line-height:24px
+						font-size:14px
+						color:rgb(7,17,27)
+					.price	
+						position:absolute
+						right:90px
+						bottom:12px
+						line-height:24px
+						font-size:14px
+						font-weight:700
+						color:rgb(240,20,20)
+					.cartcontrol-wrapper
+						position:absolute
+						right:0
+						bottom:6px
+	.list-mask
+		position:fixed
+		top:0
+		left:0
+		width:100%
+		height:100%
+		z-index:40					
+		&.fade-transition
+			opacity:1
+			background:rgba(7,17,27,0.6)
+			transition:all 0.5s				
+		&.fade-enter,&.fade-leave
+			opacity:0
+			background:rgba(7,17,27,0)	
 </style>
