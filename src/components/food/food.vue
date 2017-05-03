2bc6e3f3721a571e55cdb25672d7e59d81af7f8f
sep20th <sep20th@qq.com>
2017-05-04 06:49:07 +0800

    food组件 -格式化时间未完成

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -28,15 +28,46 @@
 					<h1 class="title">商品信息</h1>
 					<p class="text">{{food.info}}</p>
 				</div>
+				<split></split>
+				<div class="rating">
+					<h1 class="title">商品评价</h1>
+					<ratingselect :select-type='selectType' :only-content='onlyContent' :desc='desc' :ratings='food.ratings'></ratingselect>
+					<div class="rating-wrapper">
+						<!-- 此处可尝试 v-if -->
+						<ul v-show="food.ratings && food.ratings.length">
+							<!-- 通过v-show传入函数返回值,与selectType关联 -->
+							<li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+								<div class="user">
+									<span class="name">{{rating.username}}</span>
+									<img :src="rating.avatar"  class="avatar" width="12" height="12">
+								</div>
+								<!-- 使用vue的filter来格式化时间 -->
+								<div class="time">{{rating.rateTime | formatDate}}</div>
+								<p class="text">
+									<span :class="{'icon-thumb_up': rating.rateType === 0,'icon-thumb_down': rating.rateType === 1}"></span>{{rating.text}}
+								</p>
+							</li>
+						</ul>
+						<div class="no-rating" v-show=" !food.ratings || !food.ratings.length ">暂无评价</div>
+					</div>
+				</div>
 			</div>
 			</div>
 	</section>
 </template>
 
 <script>
+// import xx from ..  --对应export default
+// import { xx } from .. --对应 export function xx
 import cartcontrol from 'components/cartcontrol/cartcontrol'
 import Vue from 'vue'
+import { formatDate } from 'common/js/date'
 import split from 'components/split/split'
+import ratingselect from 'components/ratingselect/ratingselect'
+const ALL = 2;
+const POSITIVE = 0;
+const NEGATIVE = 1;
+
 	export default{
 		props:{
 			food:{
@@ -46,14 +77,23 @@ import split from 'components/split/split'
 
 		data(){
 			return{
-				showFlag:false
+				showFlag:false,
+				selectType:ALL,
+				onlyContent:true,
+				desc:{
+					all:'全部',
+					positive:'推荐',
+					negative:'吐槽'
+				}
 			}
 		},
 
 		methods:{
 			// 父组件可以调用子组件的方法,但是子组件无法调用父组件的方法
 			show(){
-				this.showFlag = true
+				this.showFlag = true,
+				this.selectType = ALL,
+				this.onlyContent = true
 			},
 
 			//返回商品页面
@@ -64,18 +104,50 @@ import split from 'components/split/split'
 			//添加购物车商品
 			addFirst(event){
 				Vue.set(this.food,'count',1)
-			}
+			},
 
+			//联动
+			needShow(type,text){
+				if (this.onlyContent && !text){
+					return false
+				}
+				if(this.selectType === ALL){
+					return true 
+				}else {
+					return type === this.selectType
+				}
+			}
+		},//methods end
+
+		//通过event监听子组件ratingselect中$dispatch的两个事件,实现商品评价类型的联动
+		events:{
+			'ratingtype.select'(type){
+				this.selectType = type
+			},
+			'content.toggle'(onlyContent){
+				this.onlyContent = onlyContent
+			}
 		},
+
+		// //定义过滤器filters
+		//  filters: {
+  //     formatDate(time) {
+  //       let date = new Date(time);
+  //       return formatDate(date, 'yyyy-MM-dd hh:mm');
+  //     }
+  //   },
+		
 
 		components:{
 			cartcontrol,
-			split
+			split,
+			ratingselect
 		}
 	}
 </script>
 
 <style lang="stylus">
+@import "../../common/stylus/mixin.styl"
 
 	.food
 		position:fixed
@@ -98,7 +170,7 @@ import split from 'components/split/split'
 			position:relative
 			width:100%
 			height:0
-			padding-top:100%  /*设置宽高相等*/
+			padding-top:100%  /*设置宽高相等的方法*/
 			img
 				position:absolute
 				top:0
@@ -172,5 +244,53 @@ import split from 'components/split/split'
 			padding 0 8px
 			font-size 12px
 			color rgb(77,85,93)	
-				
-</style>
+		.rating 
+			padding-top 18px
+			.title
+				line-height 14px
+				margin-left 18px
+				font-size 14px
+				color rgb(7,17,27)
+			.rating-wrapper
+				padding 0 18px
+				.rating-item
+					position relative
+					padding 16px 0
+					border-1px(rgba(7,17,27,0.1))	
+					.user 
+						position absolute
+						right 0px
+						top 16px
+						line-height 12px
+						font-size 0
+						.name 
+							display inline-block
+							margin-right 6px
+							vertical-align top 
+							font-size 10px
+							color rgb(147,153,159)
+						.avatar
+							border-radius 50%
+					.time 
+						margin-bottom 6px
+						line-height 12px
+						font-size 10px
+						color rgb(147,153,159)
+					.text 
+						line-height 16px
+						font-size 12px
+						color rgb(7,17,27)	
+						.icon-thumb_up,.icon-thumb_down
+							margin-right 4px
+							line-height 16px
+							font-size 12px
+						.icon-thumb_up
+							color rgb(0,160,220)
+						.icon-thumb_down
+							color rgb(147,153,159)		
+				.no-rating
+					padding 16px 0
+					font-size 12px
+					color rgb(147,153,159)
+
+</style>	
